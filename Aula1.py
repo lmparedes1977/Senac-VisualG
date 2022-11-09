918d9df3bc7d37bd90fa4618602ef9dd2f8372f6
Leonardo Paredes <110631380+lmparedes1977@users.noreply.github.com>
2022-11-08 21:53:37 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,36 @@
+"""dia = input("Dia de nascimento: ")
+ano = input("Ano de nascimento: ")
+mes = input("Mês de nascimento: ")
+
+print("Você nasceu em {:^10} de {:^10} de {:^10}." .format(dia, mes, ano) )"""
+
+"""n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+
+print(float(n1) + int(n2))
+
+algo = input("Digita algo: ")
+
+print(algo.isalnum)"""
+
+"""n1 = int(input("Digita: "))
+n2 = int(input("Digita: "))
+print("resuldade da divisao: {:.5f}".format(n1/n2))"""
+
+"""n = int(input("Digita numero "))
+print("antes de {} vem {}, depois de {} vem {}".format(n,n-1,n,n+1))"""
+
+"""n = int(input("Digita numero "))
+print("O dobro de {} é {}, o tripo é {} e {} ao quadrado é {}".format(n,n*2,n*3,n,n**(1/2)))"""
+
+"""nome = input("Digita o nome ")
+n1 = int(input("Digita numero "))
+n2 = int(input("Digita outro numero "))
+print("A media das notas do {} é {}".format(nome,(n1+n2)/2))"""
+
+"""m = float(input("Quantos centímetros? "))
+print("{} centímetros correspondem a {} milímetros".format(m,m*10))"""
+
+
+
+
